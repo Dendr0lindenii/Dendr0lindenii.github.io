d87a98c1d515b857c49dce341236f2b0f7fe3fd4
Dendr0lindenii <keishlaperez414@gmail.com>
2025-07-08 13:44:59 -0400

    Create jsDemo.js

---
+++ jsDemo.js
@@ -0,0 +1,4 @@
+console.log("This output is the from the external JS file");
+console.log("Name: Keishla Perez");
+console.log("School: Sugar Creek Charter High School");
+console.log("Grade: Senior");
