3aa5635fe37c9149ecb9d167eb2c6e92380f5279
Dendr0lindenii <keishlaperez414@gmail.com>
2025-07-15 14:50:19 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -11,3 +11,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var centralMarker = L.marker([35.2176665,-80.831473]), {title: "CPCC Central Campus"}).addTo(map);
+var harperMarker = L.marker([35.1296,-80.8956]), {title: "CPCC Harper Campus"}).addTo(map);
+var levineMarker = L.marker([35.1061,-80.6931]), {title: "CPCC Levine Campus"}).addTo(map);
+var harrisMarker = L.marker([35.22,-80.92]), {title: "CPCC Harris Campus"}).addTo(map);
+var merancasMarker = L.marker([35.39,-80.84]), {title: "CPCC Merance Campus"}).addTo(map);
+var catoMarker = L.marker([35.2648627,-80.73116]), {title: "CPCC Cato Campus"}).addTo(map);
