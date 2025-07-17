1ea932017c09d4ce50d7c8954e66c771ae8c09d7
Dendr0lindenii <keishlaperez414@gmail.com>
2025-07-17 14:42:49 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -12,12 +12,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var circleMarker01 = l.circle(coordinates. {
+var circleMarker01 = L.circle(coordinates. {
   color: "black",
   fillColor: "green",
   fillOpacity: 0.5,
   radius: 100
 }).bindPopup(`<center>
              <h3>Hello from the National Orchid Garden In Singapore!</h3>
-             <iframe width="560" height="315" src="https://www.youtube.com/embed/_pBR0yCiOX4?si=o8RZjL5H1SlhWLKu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+             <iframe width="200" height="100" src="https://www.youtube.com/embed/_pBR0yCiOX4?si=o8RZjL5H1SlhWLKu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
              </center>`).addTo(map);
